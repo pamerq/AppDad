b4f1d43fc1f18b93bcad9197050d4921f53a4964
Pame <pame@Pames-MacBook-Pro.local>
2023-03-22 22:39:03 -0500

    feat(interface): Initial Interface

--- app/src/main/java/com/example/card/MainActivity.kt
+++ app/src/main/java/com/example/card/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calcuButton: Button = findViewById(R.id.calculate)
+        calcuButton.setOnClickListener{
+
+            val montoinicialEditText: EditText = findViewById(R.id.editTextMontoInicial)
+            val inicilaEditText: EditText = findViewById(R.id.editTextInicial)
+            val cuotasEditText: EditText = findViewById(R.id.editTextCuotas)
+            val result = montoinicialEditText.text.toString().toInt() -  inicilaEditText.text.toString().toInt()
+
+
+            val toast = Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT)
+            toast.show()
+
+
+        }
     }
 }
