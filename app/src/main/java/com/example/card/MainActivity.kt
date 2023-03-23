70c7d3844b59b80f0a6f6d49c1186c933a3b26bf
Pame <pame@Pames-MacBook-Pro.local>
2023-03-22 23:38:59 -0500

    feat(interface): Add Spinner for 'Cuotas'

--- app/src/main/java/com/example/card/MainActivity.kt
+++ app/src/main/java/com/example/card/MainActivity.kt
@@ -2,22 +2,30 @@ package com.example.card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val cuotasSpinner: Spinner = findViewById(R.id.spinnerCuotas)
+        var adapterCuotas = ArrayAdapter.createFromResource(this, R.array.spinnerCuotas, android.R.layout.simple_spinner_item)
+            .also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+        }
+        cuotasSpinner.setAdapter(adapterCuotas);
+
+
         val calcuButton: Button = findViewById(R.id.calculate)
         calcuButton.setOnClickListener{
 
             val montoinicialEditText: EditText = findViewById(R.id.editTextMontoInicial)
             val inicilaEditText: EditText = findViewById(R.id.editTextInicial)
-            val cuotasEditText: EditText = findViewById(R.id.editTextCuotas)
+            val cuotasSpinner: Spinner = findViewById(R.id.spinnerCuotas)
+
             val result = montoinicialEditText.text.toString().toInt() -  inicilaEditText.text.toString().toInt()
 
 
