403c7f4ba9e830b6732237131aca1e3c062016ac
Pame <pame@Pames-MacBook-Pro.local>
2023-03-23 01:54:08 -0500

    feat(feature): Initial Operation

--- app/src/main/java/com/example/card/MainActivity.kt
+++ app/src/main/java/com/example/card/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         }
         cuotasSpinner.setAdapter(adapterCuotas);
 
-
         val calcuButton: Button = findViewById(R.id.calculate)
         calcuButton.setOnClickListener{
 
@@ -26,13 +28,28 @@ class MainActivity : AppCompatActivity() {
             val inicilaEditText: EditText = findViewById(R.id.editTextInicial)
             val cuotasSpinner: Spinner = findViewById(R.id.spinnerCuotas)
 
-            val result = montoinicialEditText.text.toString().toInt() -  inicilaEditText.text.toString().toInt()
+            val mapFactor = mapOf(
+                "12" to 0.09104638,
+                "24" to 0.04928198,
+                "36" to 0.03548678,
+                "48" to 0.02868259,//check
+                "60" to 0.02467340,
+            )
 
+            val factor = mapFactor[cuotasSpinner.getSelectedItem().toString()]
+            val result = (montoinicialEditText.text.toString().toInt() -  inicilaEditText.text.toString().toInt())*factor!!
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.CEILING
 
-            val toast = Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT)
-            toast.show()
+            //val toast = Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT)
+            //toast.show()
+
+            val resultText: TextView = findViewById(R.id.textViewResult)
+            resultText.text =  df.format(result).toDouble().toString()
 
 
         }
+
+
     }
 }
